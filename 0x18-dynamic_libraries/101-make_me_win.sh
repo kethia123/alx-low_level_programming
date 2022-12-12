57a51db9c2bd32d711b664e7560b27edb971d83a
kethia123 <kemykayigire@gmail.com>
2022-12-12 21:08:08 +0200

    Added advanced files

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P https://raw.github.com/kethia123/alx-low_level_programming/tree/master/0x18-dynamic_libraries/nrandom.so
-export LD_PRELOAD=/nrandom.so
+wget -P https://github.com/holbertonschool/0x18.c/blob/master/gm
+export LD_PRELOAD=/gm
